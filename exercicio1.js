68415e7f9893df61233db29a7d3b0f0d3bce639c
viniciusmendespinto <viniciusloco18@hotmail.com>
2017-04-11 10:27:40 -0300

    Entrega pratica de JavaScript

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,22 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let botoes = document.getElementsByClassName('botao-expandir-retrair');
+let paragrafos = document.getElementsByTagName('p');
+
+for (let i = 0; i < botoes.length; i++) { 
+
+	botoes[i].addEventListener('click', function(e){
+
+		if(botoes[i].innerHTML == "+"){
+           paragrafos[i].classList.add('expandido');
+           botoes[i].innerHTML = "-";
+		} else {
+           paragrafos[i].classList.remove('expandido');
+           botoes[i].innerHTML = "+";
+		}
+	});
+
+}
+
+
