a5b4c8ece8bc4cae9a08109199acacfd21e0cd9d
ViniciusMP <viniciusloco18@hotmail.com>
2017-04-13 00:10:52 -0300

    Exercicios 1 e 2 da página espacial feitos.

--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,46 @@
 // Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+// Este arquivo ainda não está incluído no arquivo 
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+const botaoProximo = document.getElementById('proximo');
+const botaoAnterior = document.getElementById('anterior');
+
+let fotoAtual = 0; //Imagem corrente
+let todasAsImagens = new Array();
+
+   for (let i = 0; i < 5; i++) {
+      todasAsImagens[i] = new Image();
+   }
+   todasAsImagens[0].src = "https://fegemo.github.io/cefet-web/images/philae-parts.jpg";
+   todasAsImagens[1].src = "https://fegemo.github.io/cefet-web/images/philae-rosetta.jpg";
+   todasAsImagens[2].src = "https://fegemo.github.io/cefet-web/images/philae-separation.jpg";
+   todasAsImagens[3].src = "https://fegemo.github.io/cefet-web/images/philae-67-picture.jpg";
+   todasAsImagens[4].src = "https://fegemo.github.io/cefet-web/images/philae-collecting.jpg";
+
+   botaoProximo.addEventListener('click', function(e) { 
+      fotoAtual++;
+      if (fotoAtual < todasAsImagens.length) {
+         trocaFoto(fotoAtual);
+      } else {
+      	 trocaFoto(0);
+      	 fotoAtual = 0;
+      }
+
+   });
+
+   botaoAnterior.addEventListener('click', function(e) { 
+      if (fotoAtual > 0) {
+          fotoAtual--;
+          trocaFoto(fotoAtual);
+      } else {
+      	  trocaFoto(4);
+      	  fotoAtual = 4;
+      }
+
+   });
+// Seleciona a Imagem 
+function trocaFoto(foto) {
+   document.images[1].src = todasAsImagens[foto].src;
+}
+
+
+
